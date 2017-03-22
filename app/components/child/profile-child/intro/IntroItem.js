b810ff692c9c1a773bce027481ddfef35c72ddd0
Shari Smith <Cherrybug68@gmail.com>
2017-03-22 14:50:21 -0400

    Fixed signup (added clear) and alumnihome(updated nav bar)

--- app/components/child/profile-child/intro/IntroItem.js
+++ app/components/child/profile-child/intro/IntroItem.js
@@ -3,7 +3,7 @@ var React = require("react");
 var IntroItem = React.createClass({
     render: function() {
         return (
-        <div className="row">
+        <div className="row dataStyle">
             <div className="col-md-12">
                 {/* Profile Picture */}
                 <div className="text-center">
@@ -12,8 +12,8 @@ var IntroItem = React.createClass({
                 <h2 style={{textAlign: 'center'}}>{this.props.userData.firstName + " " + this.props.userData.lastName}</h2>
                 <ul className="list-group">
                    <li className="list-group-item">Class of {this.props.userData.classOf?this.props.userData.classOf:"(not specified)"}</li>
-                   <li className="list-group-item">{(this.props.userData.address?this.props.userData.address.city + ', ':"") 
-                       + (this.props.userData.address?this.props.userData.address.state:"")}</li>
+                   <li className="list-group-item">{(this.props.userData.address && this.props.userData.address.state?this.props.userData.address.city + " and ":"") 
+                       + (this.props.userData.address && this.props.userData.address.state?this.props.userData.address.state:"")}</li>
                    <li className="list-group-item">{(this.props.userData.headline?'"' + this.props.userData.headline + '"':"")}</li>
                  </ul>
                 {/*<a className="btn btn-primary" href="#">Read More <span className="glyphicon glyphicon-chevron-right" /></a>*/}
