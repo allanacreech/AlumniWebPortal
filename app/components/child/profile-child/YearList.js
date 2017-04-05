dc3864510a070535940e99c01c95c9a1067fcf80
Shari Smith <Cherrybug68@gmail.com>
2017-04-04 22:21:15 -0400

    changed back to local mongodb link

--- app/components/child/profile-child/YearList.js
+++ app/components/child/profile-child/YearList.js
@@ -8,7 +8,7 @@ var YearList = React.createClass({
     render: function(){
         return (
         <select className="form-control" id={this.props.id} value= {this.props.year} onChange= {this.handleYearChange}>
-            {(this.props.id=="startYear"||this.props.id=="fromYear")?<option></option>:<option>Present</option>}
+            {(this.props.id=="startYear"||this.props.id=="fromYear")?<option></option>:<option>{new Date().getFullYear()}</option>}
             {this.props.years.map((year, index) => {
                 return <option key={this.props.id+index}>{year}</option>;
             })}
